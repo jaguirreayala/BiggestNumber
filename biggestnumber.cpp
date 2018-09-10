24e687bf73693e7ad56346b1dbc8510ae4a850ef
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-09-10 16:50:34 -0700

    Properly declares two variables and allows users to input whole numbes for those variables

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,18 +1,19 @@
-//Author:
+//Juan Aguirre-Ayala
 #include<iostream>
 
 int main()
 {
   //declare variables here
-
+  int wn;
+  int awn;
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>wn;
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin>>awn;
   //determine if first or second is bigger and store that in a third variable
-
+  
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
