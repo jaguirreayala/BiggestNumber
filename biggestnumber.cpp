f3e7f22cd4ef4c463a4501ca46e01a00ae8ec27d
jaguirreayala <jaguirre-ayala@mail.csuchico.edu>
2018-09-10 17:17:16 -0700

    Decides which of the two numbers are greatest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,16 +6,24 @@ int main()
   //declare variables here
   int wn;
   int awn;
-  std::cout<<"Please enter a whole number:\n";
+  int biggest;
+   std::cout << "Please enter a whole number:\n";
   //get first input using std::cin>>
-  std::cin>>wn;
-  std::cout<<"Please enter another whole number:\n";
+   std::cin >> wn;
+   std::cout << "Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-  std::cin>>awn;
+   std::cin >> awn;
   //determine if first or second is bigger and store that in a third variable
-  
-
-  std::cout<<"Of those two numbers, the biggest is: ";
+  if (wn > awn)
+   {
+    biggest = wn;
+   }
+  else
+   {
+    biggest = awn;
+   }
+   
+  std::cout << "Of those two numbers, the biggest is: " << biggest;
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
